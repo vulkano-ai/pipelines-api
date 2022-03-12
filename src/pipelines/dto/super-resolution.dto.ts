40eac62bea63687f7d6f3307e106469dbd73a606
Francesco <francescodifranco90@gmail.com>
2022-03-12 21:19:06 +0100

    fix: joi pipelines validation

--- src/pipelines/dto/super-resolution.dto.ts
+++ src/pipelines/dto/super-resolution.dto.ts
@@ -2,20 +2,16 @@ import { CREATE, getTypeSchema, JoiSchema, UPDATE } from 'nestjs-joi';
 import * as Joi from 'joi';
 
 class SuperResolutionFilterConfigDto {
-  @JoiSchema(Joi.string().valid('4/3', '3/2', '2', '3', '4'))
-  @JoiSchema([CREATE, UPDATE], Joi.string().required())
+  @JoiSchema(Joi.string().valid('4/3', '3/2', '2', '3', '4').required())
   factor!: string;
 
-  @JoiSchema(Joi.string().valid('4/3', '3/2', '2', '3', '4'))
-  @JoiSchema([CREATE, UPDATE], Joi.number().min(0).max(1).required())
+  @JoiSchema(Joi.number().min(0).max(1).required())
   strength!: number;
 }
 export class SuperResolutionFilterDto {
-  @JoiSchema(Joi.string().valid('SR'))
-  @JoiSchema([CREATE], Joi.string().required())
-  @JoiSchema([UPDATE], Joi.string().required())
+  @JoiSchema(Joi.string().valid('SR').required())
   name!: string;
 
-  @JoiSchema(getTypeSchema(SuperResolutionFilterConfigDto))
+  @JoiSchema(getTypeSchema(SuperResolutionFilterConfigDto).required())
   config!: SuperResolutionFilterConfigDto;
 }
